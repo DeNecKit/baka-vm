ac640816b726f7fca0b0fb7ef9713431c1e28c4b
deneckit <deneckitc.2004@gmail.com>
2024-02-02 20:15:12 +0300

    Seperate code into 2 files (add inst.h)

---
+++ src/inst.h
@@ -0,0 +1,69 @@
+#ifndef INST_H
+#define INST_H
+
+typedef enum {
+    INST_NOP,
+    INST_PUSH,
+    INST_GET, // absolute address
+    INST_DUP, // relative address
+    INST_SET,
+    INST_IADD,
+    INST_ISUB,
+    INST_IMUL,
+    INST_IDIV,
+    INST_IEQ,
+    INST_IGT,
+    INST_IGE,
+    INST_ILT,
+    INST_ILE,
+    INST_JMP,
+    INST_JNZ,    
+    INST_HALT,
+    INST_PRINT_INT // temporary for debug purposes
+} inst_type_t;
+
+const char *inst_type_to_cstr(inst_type_t type)
+{
+    switch (type) {
+    case INST_NOP: return "INST_NOP";
+    case INST_PUSH: return "INST_PUSH";
+    case INST_GET: return "INST_GET";
+    case INST_DUP: return "INST_DUP";
+    case INST_SET: return "INST_SET";
+    case INST_IADD: return "INST_IADD";
+    case INST_ISUB: return "INST_ISUB";
+    case INST_IMUL: return "INST_IMUL";
+    case INST_IDIV: return "INST_IDIV";
+    case INST_IEQ: return "INST_IEQ";
+    case INST_IGT: return "INST_IGT";
+    case INST_IGE: return "INST_IGE";
+    case INST_ILT: return "INST_ILT";
+    case INST_ILE: return "INST_ILE";
+    case INST_JMP: return "INST_JMP";
+    case INST_JNZ: return "INST_JNZ";
+    case INST_HALT: return "INST_HALT";
+    case INST_PRINT_INT: return "INST_PRINT_INT";
+    default: assert(0 && "unreachable");
+    }
+}
+
+#define MAKE_NOP (inst_t) { .inst_type = INST_NOP }
+#define MAKE_PUSH_I64(opr) (inst_t) { .type = INST_PUSH, .op = { .as_i64 = (opr) } }
+#define MAKE_GET(opr) (inst_t) { .type = INST_GET, .op = { .as_u64 = (opr) } }
+#define MAKE_DUP(opr) (inst_t) { .type = INST_DUP, .op = { .as_u64 = (opr) } }
+#define MAKE_SET(opr) (inst_t) { .type = INST_SET, .op = { .as_u64 = (opr) } }
+#define MAKE_IADD (inst_t) { .type = INST_IADD }
+#define MAKE_ISUB (inst_t) { .type = INST_ISUB }
+#define MAKE_IMUL (inst_t) { .type = INST_IMUL }
+#define MAKE_IDIV (inst_t) { .type = INST_IDIV }
+#define MAKE_IEQ (inst_t) { .type = INST_IEQ }
+#define MAKE_IGT (inst_t) { .type = INST_IGT }
+#define MAKE_IGE (inst_t) { .type = INST_IGE }
+#define MAKE_ILT (inst_t) { .type = INST_ILT }
+#define MAKE_ILE (inst_t) { .type = INST_ILE }
+#define MAKE_JMP(opr) (inst_t) { .type = INST_JMP, .op = { .as_u64 = (opr) } }
+#define MAKE_JNZ(opr) (inst_t) { .type = INST_JNZ, .op = { .as_u64 = (opr) } }
+#define MAKE_HALT (inst_t) { .type = INST_HALT }
+#define MAKE_PRINT_INT (inst_t) { .type = INST_PRINT_INT }
+
+#endif // INST_H
--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
+#include "inst.h"
     
 typedef uint8_t byte_t;
 typedef union {
@@ -10,57 +12,10 @@ typedef union {
     double as_f64;
 } word_t;
 
-typedef enum {
-    INST_NOP,
-    INST_PUSH,
-    INST_GET, // absolute address
-    INST_DUP, // relative address
-    INST_SET,
-    INST_IADD,
-    INST_ISUB,
-    INST_IMUL,
-    INST_IDIV,
-    INST_IEQ,
-    INST_IGT,
-    INST_IGE,
-    INST_ILT,
-    INST_ILE,
-    INST_JMP,
-    INST_JNZ,    
-    INST_HALT,
-    INST_PRINT_INT // temporary for debug purposes
-} inst_type_t;
-
 typedef struct {
     byte_t type;
     word_t op;
 } inst_t;
-
-const char *inst_type_to_cstr(inst_type_t type)
-{
-    switch (type) {
-    case INST_NOP: return "INST_NOP";
-    case INST_PUSH: return "INST_PUSH";
-    case INST_GET: return "INST_GET";
-    case INST_DUP: return "INST_DUP";
-    case INST_SET: return "INST_SET";
-    case INST_IADD: return "INST_IADD";
-    case INST_ISUB: return "INST_ISUB";
-    case INST_IMUL: return "INST_IMUL";
-    case INST_IDIV: return "INST_IDIV";
-    case INST_IEQ: return "INST_IEQ";
-    case INST_IGT: return "INST_IGT";
-    case INST_IGE: return "INST_IGE";
-    case INST_ILT: return "INST_ILT";
-    case INST_ILE: return "INST_ILE";
-    case INST_JMP: return "INST_JMP";
-    case INST_JNZ: return "INST_JNZ";
-    case INST_HALT: return "INST_HALT";
-    case INST_PRINT_INT: return "INST_PRINT_INT";
-    default: assert(0 && "unreachable");
-    }
-
-}
 
 #define MAX_STACK_SIZE 1024
 word_t stack[MAX_STACK_SIZE] = {0};
@@ -113,25 +68,6 @@ void add_inst(inst_t inst)
     assert(program_size < MAX_PROGRAM_SIZE);
     program[program_size++] = inst;
 }
-
-#define MAKE_NOP (inst_t) { .inst_type = INST_NOP }
-#define MAKE_PUSH_I64(opr) (inst_t) { .type = INST_PUSH, .op = { .as_i64 = (opr) } }
-#define MAKE_GET(opr) (inst_t) { .type = INST_GET, .op = { .as_u64 = (opr) } }
-#define MAKE_DUP(opr) (inst_t) { .type = INST_DUP, .op = { .as_u64 = (opr) } }
-#define MAKE_SET(opr) (inst_t) { .type = INST_SET, .op = { .as_u64 = (opr) } }
-#define MAKE_IADD (inst_t) { .type = INST_IADD }
-#define MAKE_ISUB (inst_t) { .type = INST_ISUB }
-#define MAKE_IMUL (inst_t) { .type = INST_IMUL }
-#define MAKE_IDIV (inst_t) { .type = INST_IDIV }
-#define MAKE_IEQ (inst_t) { .type = INST_IEQ }
-#define MAKE_IGT (inst_t) { .type = INST_IGT }
-#define MAKE_IGE (inst_t) { .type = INST_IGE }
-#define MAKE_ILT (inst_t) { .type = INST_ILT }
-#define MAKE_ILE (inst_t) { .type = INST_ILE }
-#define MAKE_JMP(opr) (inst_t) { .type = INST_JMP, .op = { .as_u64 = (opr) } }
-#define MAKE_JNZ(opr) (inst_t) { .type = INST_JNZ, .op = { .as_u64 = (opr) } }
-#define MAKE_HALT (inst_t) { .type = INST_HALT }
-#define MAKE_PRINT_INT (inst_t) { .type = INST_PRINT_INT }
 
 int main(void)
 {
